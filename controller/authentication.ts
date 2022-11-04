8c1de0afa05abe3f55155b474032b496bd816b1b
musharaf-fleapo <musharaf@fleapo.com>
2022-11-04 16:41:21 +0530

    small bug fixed

--- controller/authentication.ts
+++ controller/authentication.ts
@@ -18,7 +18,7 @@ const login = async (req: Request, res: Response) => {
           .status(200)
           .json({ err: false, message: "User Logged in successfully", token });
       }
-      res
+      return res
         .status(404)
         .json({ error: true, message: "Invalid Email and Password" });
     }
@@ -39,6 +39,8 @@ const getUserProfile = async (req: any, res: Response) => {
         error: false,
         message: "User Profile retrieved Successfully",
         data: user,
+        totalFriends: user.friends.length,
+        totalReq: user.friendRes.length,
       });
     }
     res.status(404).json({ error: true, message: "No Such User Found" });
